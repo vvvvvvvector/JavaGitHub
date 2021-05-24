701e35fef21ead237a5a2ae69844c332539bf8fa
vvvvvvvector <57532024+vvvvvvvector@users.noreply.github.com>
2021-05-24 18:38:26 +0100

    lb11_breakout, zadanie3, +ball, now ball movement depends on mouse movement

---
+++ lb11_breakout/src/sample/Ball.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Ball extends GraphicsItem {
+    private Point2D moveVector = new Point2D(1, -1).normalize(); // 45 degrees corner
+    private double velocity = 10;
+
+    public Ball() {
+        width = height = canvasHeight * 0.03;
+    }
+
+    @Override
+    public void draw(GraphicsContext graphicsContext) {
+        graphicsContext.setFill(Color.WHITE);
+        graphicsContext.fillOval(x, y, width, height);
+    }
+
+    public void setPosition(Point2D point2D) {
+        this.x = point2D.getX() - width / 2;
+        this.y = point2D.getY() - width / 2 - 8;
+    }
+
+    public void updatePosition(){
+        this.x += moveVector.getX() * velocity;
+        this.y += moveVector.getY() * velocity;
+    }
+}
--- lb11_breakout/src/sample/GameCanvas.java
+++ lb11_breakout/src/sample/GameCanvas.java
@@ -1,22 +1,27 @@
 package sample;
 
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class GameCanvas extends Canvas {
+    private boolean isGameRunning = false;
     private GraphicsContext graphicsContext = this.getGraphicsContext2D();
     private Paddle paddle;
+    private Ball ball;
 
     public void initialize() {
         paddle = new Paddle();
+        ball = new Ball();
     }
 
     public void draw() {
         graphicsContext.setFill(Color.BLACK); // GameCanvas color
-        graphicsContext.fillRect(0, 0, getWidth(), getHeight()); // GameCanvas size
-        paddle.draw(graphicsContext); //drawing paddle
+        graphicsContext.fillRect(0, 0, getWidth(), getHeight()); // drawing GameCanvas
+        paddle.draw(graphicsContext); // drawing paddle
+        ball.draw(graphicsContext); // drawing ball
     }
 
     public GameCanvas() {
@@ -26,6 +31,15 @@ public class GameCanvas extends Canvas {
         this.setOnMouseMoved(mouseEvent -> {
             paddle.setPosition(mouseEvent.getX());
             draw();
+            if(!isGameRunning){
+                ball.setPosition(new Point2D(mouseEvent.getX(), paddle.getY()));
+            } else {
+                ball.updatePosition();
+            }
+        });
+
+        this.setOnMouseClicked(mouseClickEvent -> {
+            isGameRunning = true;
         });
     }
 }
