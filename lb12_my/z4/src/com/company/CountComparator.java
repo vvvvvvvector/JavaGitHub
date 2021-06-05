ab029a6c8b193c01365052d14f3646fa65377721
vvvvvvvector <57532024+vvvvvvvector@users.noreply.github.com>
2021-06-05 22:30:26 +0100

    lb12_my, zadanie4: +CountComparator, +SumComparator

---
+++ lb12_my/z4/src/com/company/CountComparator.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Collection;
+import java.util.Comparator;
+
+public class CountComparator<T extends Collection<?>> implements Comparator<T> {
+    @Override
+    public int compare(T o1, T o2) {
+        return o1.size() - o2.size();
+    }
+}
--- lb12_my/z4/src/com/company/Main.java
+++ lb12_my/z4/src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world!(4)");
+        List<Integer> list1 = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7));
+        List<Integer> list2 = new ArrayList<>(List.of(1, 2, 3));
+        CountComparator<List<Integer>> countComparator = new CountComparator<>();
+        System.out.println(countComparator.compare(list1, list2));
+
+        SumComparator<List<Integer>, Integer> sumComparator = new SumComparator<>();
+        System.out.println(sumComparator.compare(list2, list1));
     }
 }
