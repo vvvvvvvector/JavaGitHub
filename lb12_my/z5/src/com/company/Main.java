ccb084e864efff4c2a461af278a06c56c06020b9
vvvvvvvector <57532024+vvvvvvvector@users.noreply.github.com>
2021-06-06 10:39:08 +0100

    lb12_my, zadanie5: +addAtPosition in List<T> and T[]

--- lb12_my/z5/src/com/company/Main.java
+++ lb12_my/z5/src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world!(5)");
+        List<Integer> list = new ArrayList<>(List.of(1, 2, 3, 4, 5));
+        list = addAtPosition(list, 10, 1);
+        list.forEach(el -> System.out.print(el + " "));
+        System.out.println("\n-------------");
+        Double[] arr = new Double[]{1.0, 2.0, 3.0, 4.0, 5.1};
+        arr = addAtPosition(arr, 10.0, 1);
+        for (var i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    // T - "typ szablanowy"
+    public static <T> List<T> addAtPosition(List<T> list, T el, int pos) {
+        List<T> resultList = new ArrayList<>(list);
+        resultList.add(pos, el);
+        return resultList;
+    }
+
+    public static <T> T[] addAtPosition(T[] arr, T el, int pos) {
+        T[] resultArray = Arrays.copyOf(arr, arr.length + 1);
+        resultArray[pos] = el;
+        for (int i = pos + 1; i < arr.length + 1; i++) {
+            resultArray[i] = arr[i - 1];
+        }
+        return resultArray;
     }
 }
